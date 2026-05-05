b220825b7d8cd0e13524f1ef9773dfe0eec19982
AMV <amv.prog@gmail.com>
2026-05-05 09:17:50 +0200

    init accueil et footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'amv-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/layout/footer/footer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'amv-footer',
+  imports: [RouterLink],
+  templateUrl: './footer.html',
+})
+export class Footer {}
