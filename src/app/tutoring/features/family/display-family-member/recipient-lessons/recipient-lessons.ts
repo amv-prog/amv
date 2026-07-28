8fbeb3c53e6e90c6c836ff2aa12173acbbb4d0db
AMV <amv.prog@gmail.com>
2026-07-28 14:00:34 +0200

    refacto affichage cours membre

---
+++ src/app/tutoring/features/family/display-family-member/recipient-lessons/recipient-lessons.ts
@@ -0,0 +1,130 @@
+import { Dialog } from '@angular/cdk/dialog';
+import { DatePipe } from '@angular/common';
+import { Component, computed, inject, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { tap } from 'rxjs';
+import { ConfirmationDialog } from '../../../../../shared/components/confirmation-dialog/confirmation-dialog';
+import { DateService } from '../../../../../shared/services/date-service';
+import { Lesson } from '../../../../models/lesson';
+import { RecipientMember } from '../../../../models/recipient-member';
+import { VolunteerMember } from '../../../../models/volunteer-member';
+import { LessonStore } from '../../../../stores/lesson-store';
+import { VolunteerStore } from '../../../../stores/volunteer-store';
+
+@Component({
+  selector: 'amv-recipient-lessons',
+  imports: [DatePipe, RouterLink],
+  templateUrl: './recipient-lessons.html',
+})
+export class RecipientLessons {
+  public readonly member = input.required<RecipientMember>();
+
+  private readonly volunteerStore = inject(VolunteerStore);
+  private readonly lessonStore = inject(LessonStore);
+  private readonly dateService = inject(DateService);
+
+  private readonly dialog = inject(Dialog);
+
+  protected readonly daysMap = this.dateService.daysMap;
+
+  protected readonly currentLessonsToDisplay = computed(() => {
+    const tutorsMap = this.volunteerStore.tutorsMap();
+
+    return this.lessonStore
+      .lessons()
+      .filter((lesson) => lesson.studentId === this.member()?.id)
+      .filter((lesson) => {
+        return !lesson.endDate || DateService.compareDays(lesson.endDate, new Date()) >= 0;
+      })
+      .filter((lesson) => {
+        return DateService.compareDays(lesson.startDate, new Date()) <= 0;
+      })
+      .sort(LessonStore.compareLessonsDays)
+      .map((lesson) => {
+        const tutor = tutorsMap.get(lesson.tutorId);
+
+        return { lesson, tutor };
+      });
+  });
+
+  protected readonly pastLessonsToDisplay = computed(() => {
+    const tutorsMap = this.volunteerStore.tutorsMap();
+
+    return this.lessonStore
+      .lessons()
+      .filter((lesson) => lesson.studentId === this.member()?.id)
+      .filter((lesson) => {
+        return !!lesson.endDate && DateService.compareDays(lesson.endDate, new Date()) < 0;
+      })
+      .sort(
+        (l1, l2) =>
+          LessonStore.compareLessonsEndDate(l1, l2) || LessonStore.compareLessonsDays(l1, l2),
+      )
+      .map((lesson) => {
+        const tutor = tutorsMap.get(lesson.tutorId);
+
+        return { lesson, tutor };
+      });
+  });
+
+  protected readonly futureLessonsToDisplay = computed(() => {
+    const tutorsMap = this.volunteerStore.tutorsMap();
+
+    return this.lessonStore
+      .lessons()
+      .filter((lesson) => lesson.studentId === this.member()?.id)
+      .filter((lesson) => {
+        return DateService.compareDays(lesson.startDate, new Date()) > 0;
+      })
+      .sort(
+        (l1, l2) =>
+          LessonStore.compareLessonsStartDate(l1, l2) || LessonStore.compareLessonsDays(l1, l2),
+      )
+      .map((lesson) => {
+        const tutor = tutorsMap.get(lesson.tutorId);
+
+        return { lesson, tutor };
+      });
+  });
+
+  public displayTutorName(volunteer: VolunteerMember | undefined): string {
+    return !!volunteer ? LessonStore.displayTutorName(volunteer) : 'Inconnu';
+  }
+
+  stopLesson(lesson: Lesson) {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    this.lessonStore.updateLesson({ ...lesson, endDate: this.dateService.formatDate(today) });
+  }
+
+  reactivateLesson(lesson: Lesson) {
+    this.lessonStore.updateLesson({ ...lesson, endDate: undefined });
+  }
+
+  canStopLesson(lesson: Lesson) {
+    return !lesson.endDate || DateService.stringToDate(lesson.endDate)! > new Date();
+  }
+
+  public validateLessonRemoval(lesson: Lesson) {
+    const dialogRef = this.dialog.open<boolean>(ConfirmationDialog, {
+      panelClass: 'dialog',
+      data: {
+        text: `Souhaitez-vous supprimer ce cours ? Attention, il n'apparaîtra plus dans l'historique.`,
+      },
+    });
+
+    dialogRef.closed
+      .pipe(
+        tap((result) => {
+          if (!!result) {
+            this.removeLesson(lesson);
+          }
+        }),
+      )
+      .subscribe();
+  }
+
+  private removeLesson(lesson: Lesson) {
+    this.lessonStore.removeLesson(lesson);
+  }
+}
